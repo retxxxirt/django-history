f84888c938cd338575b4e69ee8ee0308074d3319
retxxxirt <retxxxirt@gmail.com>
2020-03-15 01:28:56 +0800

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='django-history-manager',
+    packages=['django_history'],
+    package_data={'django_history': ['migrations/*.py']},
+    version='0.1.0',
+    license='MIT',
+    description='History manger for django models.',
+    # long_description=open('README.md').read(),
+    # long_description_content_type='text/markdown',
+    author='retxxxirt',
+    author_email='retxxirt@gmail.com',
+    url='https://github.com/retxxxirt/django-history',
+    keywords=['django history', 'django', 'django history manager', 'django history model', 'django history field'],
+    tests_require=['django==3.0.4']
+)
